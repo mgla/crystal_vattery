46a4ba424fb947e3df331b405ed385c40e0746dc
Maik Glatki <maik@glatki.eu>
2024-03-04 23:40:47 +0100

    feat: lua event handler now working, althoug incomplete. added material for metallic crystals. vattery now uses magma

--- scripts_modactive/crystal-vattery-replace-wood.lua
+++ scripts_modactive/crystal-vattery-replace-wood.lua
@@ -1,29 +1,37 @@
 --@module = true
 --@enable = true
 
-print('cw main')
+print('cv main')
 
 local eventful = require("plugins.eventful")
 local modId = "crystal-vattery"
+local replacement_materials = {
+    ["CV_AUSTENITE_CRYSTAL"] = "CV_AUSTENITE_CRYSTAL"
+}
 
 -- From https://github.com/DFHack/dfhack/blob/develop/plugins/lua/eventful.lua#L152C11-L152C22
 eventful.enableEvent(eventful.eventType.ITEM_CREATED, 1)
 
 eventful.onItemCreated[modId] = function(item_id)
-    print('cw onItemCreated')
-    print(item_id)
+    print('cv: onItemCreated' + 'id: ' + item_id)
+
+    -- get item reference from id
+    local item = df.item.find(item_id)
+    -- check if material is in the list for this mod, if yes, replace
+    local material = (dfhack.matinfo.decode(item.mat_type,item.mat_index).plant.id)
+    print("cv: material: " + material)
+    if replacement_materials[material] then
+        local pos = dfhack.items.getPosition(item)
+        dfhack.items.remove(item)
+        -- see https://github.com/DFHack/scripts/blob/master/modtools/create-item.lua
+        -- and https://github.com/DFHack/scripts/blob/master/gui/create-item.lua
+        -- script.start(createitem.hackWish, default_accessors, opts) or something  
+        -- modtools/create-item -i BAR:NONE -m INORGANIC:CV_AUSTENITE
+        print('will replace item')
+    end
 end
 
 -- mod enables itself
 function isEnabled()
     return true
 end
-
-
-
--- enableEvent(evType,frequency)
-
--- Enable event checking for EventManager events. For event types use eventType table.
--- Note that different types of events require different frequencies to be effective.
--- The frequency is how many ticks EventManager will wait before checking if that type of event has happened.
--- If multiple scripts or plugins use the same event type, the smallest frequency is the one that is used, so you might get events triggered more often than the frequency you use here.
